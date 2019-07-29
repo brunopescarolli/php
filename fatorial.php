f33f6ef26a590c2965f27413836f6a0744817549
brunopescarolli <bruno.pescarolli@outlook.com>
2019-07-29 10:01:05 -0300

    php

---
+++ fatorial.php
@@ -0,0 +1,9 @@
+<?php
+$N=readline("Digite um valor para mostar seu fatorial: ");
+$n=$N;
+$f=1;
+while $n>0;
+    $f=$f*$n;
+    $n=$n-1;
+echo "O fatorial de $N é $f"
+?>
---
+++ tabuada.php
@@ -0,0 +1,7 @@
+<?php 
+$x=1;
+$valor=readline("Digite um valor para ser mostrado a sua tabuada: ");
+while x<=1;
+    echo "$valor x $x = $valor*$x";
+    $x++;
+?>
